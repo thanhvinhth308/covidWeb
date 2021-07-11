f9d0d35e10adc9f5b95dd2f685db72bfc8e83998
vinh-le <thanhvinhth308@gmail.com>
2021-07-11 09:16:49 +0700

    first commit test

--- src/App.js
+++ src/App.js
@@ -14,6 +14,7 @@ import Register from './pages/Register';
 function App() {
   return (
     <div className="App">
+      test commit
       {/* <GlobalLoading /> */}
       <Switch>
         <PrivateRouter exact path="/" component={Home} />
